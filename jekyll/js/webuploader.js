b28758eaaa15f768faf0f0ea5a38c61fb51932c2
QingDengYue <zrl412@163.com>
2017-05-22 21:11:40 +0800

    regenerated

--- jekyll/js/webuploader.js
+++ jekyll/js/webuploader.js
@@ -173,6 +173,7 @@
         };
     });
     /**
+     * 同jq-bridge,在没有jquery的时候才需要，用来实现Deferred
      * @fileOverview Promise/A+
      */
     define('promise',[
@@ -354,8 +355,10 @@
                     child = protos;
                     protos = null;
                 } else if ( protos && protos.hasOwnProperty('constructor') ) {
+                    //如果子类存在构造器则实用子类的构造器
                     child = protos.constructor;
                 } else {
+                    //调用父类
                     child = function() {
                         return Super.apply( this, arguments );
                     };
@@ -936,9 +939,11 @@
     
         // 接口类。
         function Runtime( options ) {
+            //默认container为document.body
             this.options = $.extend({
                 container: document.body
             }, options );
+            //生成uid
             this.uid = Base.guid('rt_');
         }
     
@@ -1024,6 +1029,7 @@
     });
     
     /**
+     * 连接器
      * @fileOverview Runtime管理器，负责Runtime的选择, 连接
      */
     define('runtime/client',[
@@ -1163,6 +1169,7 @@
         return RuntimeClient;
     });
     /**
+     * 文件拖拽
      * @fileOverview 错误信息
      */
     define('lib/dnd',[
@@ -1208,6 +1215,7 @@
         return DragAndDrop;
     });
     /**
+     * 实现command机制
      * @fileOverview 组件基类。
      */
     define('widgets/widget',[
@@ -1447,6 +1455,7 @@
         return Widget;
     });
     /**
+     * 文件拖拽应用在Uploader
      * @fileOverview DragAndDrop Widget。
      */
     define('widgets/filednd',[
@@ -1520,6 +1529,8 @@
     });
     
     /**
+     *
+     * 负责图片黏贴
      * @fileOverview 错误信息
      */
     define('lib/filepaste',[
@@ -1554,6 +1565,7 @@
         return FilePaste;
     });
     /**
+     * 图片粘贴应用在Uploader
      * @fileOverview 组件基类。
      */
     define('widgets/filepaste',[
@@ -1603,7 +1615,7 @@
             }
         });
     });
-    /**
+    /**带ruid（为了兼容flash抽象出来的，ruid为运行时id）的Blob类
      * @fileOverview Blob
      */
     define('lib/blob',[
@@ -1649,6 +1661,7 @@
         return Blob;
     });
     /**
+     * 带ruid的文件类，blob的子类
      * 为了统一化Flash的File和HTML5的File而存在。
      * 以至于要调用Flash里面的File，也可以像调用HTML5版本的File一下。
      * @fileOverview File
@@ -1686,6 +1699,7 @@
     });
     
     /**
+     * 文件选择器
      * @fileOverview 错误信息
      */
     define('lib/filepicker',[
@@ -1823,6 +1837,7 @@
     });
     
     /**
+     * 文件上传应用在Uploader
      * @fileOverview 文件选择相关
      */
     define('widgets/filepicker',[
@@ -1967,6 +1982,7 @@
         });
     });
     /**
+     * 图片处理类，生成缩略图和图片压缩
      * @fileOverview Image
      */
     define('lib/image',[
@@ -2065,6 +2081,7 @@
         return Image;
     });
     /**
+     * 图片文件在对应的时机做图片压缩和预览
      * @fileOverview 图片操作, 负责预览图片和上传前压缩图片
      */
     define('widgets/image',[
@@ -2316,6 +2333,7 @@
                 });
                 image.once( 'error', deferred.reject );
                 image.once( 'load', function() {
+                    
                     var width = opts.width,
                         height = opts.height;
     
@@ -2374,6 +2392,7 @@
         });
     });
     /**
+     * 文件类，Queue中存放的数据类
      * @fileOverview 文件属性封装
      */
     define('file',[
@@ -2802,6 +2821,7 @@
     });
     
     /**
+     * 队列管理
      * @fileOverview 队列
      */
     define('widgets/queue',[
@@ -3111,6 +3131,7 @@
     
     });
     /**
+     * 添加runtime信息给Uploader
      * @fileOverview 添加获取Runtime相关信息的方法。
      */
     define('widgets/runtime',[
@@ -3168,6 +3189,8 @@
         });
     });
     /**
+     *
+     * 文件传送
      * @fileOverview Transport
      */
     define('lib/transport',[
@@ -3298,6 +3321,7 @@
         return Transport;
     });
     /**
+     * 负责具体的上传逻辑哦
      * @fileOverview 负责文件上传相关。
      */
     define('widgets/upload',[
@@ -4149,6 +4173,7 @@
     });
     
     /**
+     * 各种验证器
      * @fileOverview 各种验证，包括文件总大小是否超出、单文件是否超出和文件是否重复。
      */
     
@@ -4491,6 +4516,7 @@
         });
     });
     /**
+     * Component基类
      * @fileOverview Runtime管理器，负责Runtime的选择, 连接
      */
     define('runtime/compbase',[],function() {
